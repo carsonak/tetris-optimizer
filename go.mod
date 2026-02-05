ab403fc75429644bb5716aece494a506c895edec
Andrew <carsoniskihara@gmail.com>
2026-02-05 18:23:43 +0300

    First

---
+++ go.mod
@@ -0,0 +1,3 @@
+module tetris-optimiser
+
+go 1.24.3
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "ERROR: %s tetromino_file", os.Args[0])
+		os.Exit(1)
+	}
+}
