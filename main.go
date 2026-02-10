47dc8eb04f8b8442409f2b0aee3ae4f5d25ff5bd
Andrew <carsoniskihara@gmail.com>
2026-02-10 19:42:34 +0300

    WIP: working on solver

--- main.go
+++ main.go
@@ -2,9 +2,33 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
+
+	"tetris-optimiser/tetromino"
 )
+
+func initTetrominoPieces(rawTetrominoes []tetromino.Raw) ([]tetromino.Piece, error) {
+	var tetrominoes []tetromino.Piece
+
+	for i, t := range rawTetrominoes {
+		id := rune('A' + i)
+
+		if id > 'Z' { // Since IDs are only uppercase letters, number of tetrominoes is capped at 26
+			return nil, errors.New("ERROR: cannot process more than 26 tetrominoes")
+		}
+
+		piece, err := tetromino.Init(t, id)
+		if err != nil {
+			return nil, err
+		}
+
+		tetrominoes = append(tetrominoes, piece)
+	}
+
+	return tetrominoes, nil
+}
 
 func main() {
 	if len(os.Args) != 2 {
@@ -14,13 +38,19 @@ func main() {
 
 	file, err := os.Open(os.Args[1])
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "ERROR: %v", err)
+		fmt.Fprintf(os.Stderr, "ERROR: %v\n", err)
 		os.Exit(1)
 	}
 
 	defer file.Close()
-	_, err = ParseTetrominoStream(bufio.NewScanner(file))
+	rawTetrominoes, err := ParseTetrominoStream(bufio.NewScanner(file))
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "ERROR: %v", err)
+		fmt.Fprintf(os.Stderr, "ERROR: %v\n", err)
+		os.Exit(1)
+	}
+
+	_, err = initTetrominoPieces(rawTetrominoes)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR: %v\n", err)
 	}
 }
---
+++ solve.go
@@ -0,0 +1,5 @@
+package main
+
+import "tetris-optimiser/tetromino"
+
+func solve(tets []tetromino.Piece, grid [][]rune) {}
